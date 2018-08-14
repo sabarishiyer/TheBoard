250c64817eef39a076de0857d7d27bb23cbe1db5
sabarishiyer <37049307+sabarishiyer@users.noreply.github.com>
2018-08-14 08:34:33 +0100

    +initial files

---
+++ server.js
@@ -0,0 +1,15 @@
+﻿//var http = require('http');
+//var port = process.env.port || 1337;
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+var http = require("http");
+var server = http.createServer(function (req, res) {
+    console.log(req.url);
+    res.write("<html> <h1> " + req.url +  "</h1> </html>");
+    res.end();
+});
+
+server.listen(3000);
