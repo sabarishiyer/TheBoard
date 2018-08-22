02b5653f60c979661844a9e2954f9d309e9c04de
sabarishiyer <37049307+sabarishiyer@users.noreply.github.com>
2018-08-22 14:04:26 +0100

    Add files via upload

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -9,10 +9,18 @@
             data.getNoteCategories(function (err, results) {
                 res.render("index", { title: "Express + Vash",error:err,categories:results });
                 res.send(results);
-            })
-
-            
+            })    
         });
+
+
+        app.post("/api/contact/:contactName", function (req, res) {
+            res.set("Content-Type", "application/json");
+            var contactName = req.params.contactName;
+            console.log(contactName);
+            console.log(req.body.email);
+            res.send({"success":"sent from server"});
+        })
+
     };
 
 })(module.exports);
