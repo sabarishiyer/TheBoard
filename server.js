8b8118fbbc4d6d7c82fe90611fd38f2c90b7ad52
sabarishiyer <37049307+sabarishiyer@users.noreply.github.com>
2018-08-22 07:23:18 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,10 +7,13 @@
 
 var http = require("http");
 var express = require("express");
+var bodyParser = require('body-parser');
 var controllers = require("./controllers");
 var app = express();
 
 
+app.use(bodyParser.urlencoded({ entended :false}));
+app.use(bodyParser.json());
 app.set("view engine", "vash");
 
 
@@ -18,6 +21,5 @@ app.set("view engine", "vash");
 controllers.init(app);
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000.');
+    console.log('Listening on port 3000.');
 })
-
